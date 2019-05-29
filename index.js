a1c333e2ff525276a671a60ac485182cc8373191
Jason Le <jas0nle@yahoo.com>
2019-05-29 19:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,5 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var Array = kittens;
-  Array.concat(kittens, name);
-  return Array;
+ return (...kittens, name);
 }
